e6068bca82812f149480b65e08532a7fb1b4f195
morrillo <gustavo.orrillo@moldeo.coop>
2015-08-01 10:30:57 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Partner Label Report',
+    'category': 'Sales',
+    'version': '1.1',
+    'depends': ['crm', 'base','sale'],
+    'data': [
+        'label_report.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
